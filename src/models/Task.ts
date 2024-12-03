28ee4221b251c766b0e5e2dbc12d44f9783e24b7
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-12-03 10:07:34 +0700

    additional filed in db for schema tasks

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -10,6 +10,54 @@ const TaskSchema = new Schema(
       default: "Pending",
     },
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High", "Critical"],
+      default: "Medium",
+    },
+    dueDate: { type: Date },
+    proress: { type: Number, min: 0, max: 0, default: 0 },
+    subTasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+    assignees: [
+      {
+        userId: { type: Schema.Types.ObjectId, ref: "User" },
+        role: {
+          type: String,
+          enum: ["Owner", "Collaborator", "Viewer"],
+          default: "Collaborator",
+        },
+        assignedAt: { type: Date, default: () => new Date().toISOString() },
+      },
+    ],
+    statusHistory: [
+      {
+        status: {
+          type: String,
+        },
+        updatedAt: { type: Date, default: () => new Date().toISOString() },
+      },
+    ],
+    tags: [{ type: String }],
+    comments: [
+      {
+        content: { type: String },
+        createdBy: { type: Schema.Types.ObjectId, ref: "User" },
+        createdAt: { type: Date, default: () => new Date().toISOString() },
+      },
+    ],
+    attachments: [{ type: String }],
+    customFields: [
+      {
+        fieldName: { type: String },
+        value: { type: String },
+      },
+    ],
+    isArchived: { type: Boolean, default: false },
+    visibility: {
+      type: String,
+      enum: ["Public", "Private", "Team"],
+      default: "Private",
+    },
   },
   { timestamps: true } // Tự động thêm createdAt và updatedAt
 );
